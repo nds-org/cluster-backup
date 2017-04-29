e66bd001a79ddbffd233afd77db8c4a48fe0c734
Mike Lambert <lambert8@illinois.edu>
2017-04-29 12:38:12 -0500

    Added simple query/restore scripts (currently broken, still testing)

---
+++ restore.sh
@@ -0,0 +1,23 @@
+core@lambert8-dev ~/cluster-backup $ cat list-backups.sh 
+#!/bin/bash
+[ $DEBUG ] && set -x
+
+# XXX: Set this to "echo" to for a dry-run
+DEBUG=""
+
+# Set some basic
+MYADDR=$(ip addr show eth0 scope global | grep inet | tr -s ' ' | cut -d' ' -f3 | cut -d/ -f1)
+DATE=$(date +%y-%m-%d.%H%M)
+TARGET_PATH=${BACKUP_DEST:-/ndsbackup}/${HOSTNAME:-localhost}
+
+echo "Backup started: ${DATE}"
+
+# Use the above to build our base commands
+SSH_ARGS="-i ${BACKUP_KEY:-backup.pem} -o StrictHostKeyChecking=no "
+SSH_TARGET="${BACKUP_USER:-centos}@${BACKUP_HOST:-localhost}"
+
+# Retrieve contents of remote backup from the given string
+$DEBUG ssh ${SSH_ARGS} ${SSH_TARGET} "ls -al ${TARGET_PATH}" | grep $1 | scp ${SSH_ARGS} ${SSH_TARGET}:- $HOME 
+
+
+
